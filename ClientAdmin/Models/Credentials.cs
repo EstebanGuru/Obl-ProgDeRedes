d03f75c5aae8d2601f7eb197edb4b3dfb97d5084
Esteban <esteban.guru@gmail.com>
2019-11-12 23:58:12 -0300

    integracion cambios.

--- ClientAdmin/Models/Credentials.cs
+++ ClientAdmin/Models/Credentials.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HttpClientApp.Models
+namespace ClientAdmin.Models
 {
     public class Credentials
     {
--- ClientAdmin/Program.cs
+++ ClientAdmin/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClientAdmin.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -7,27 +8,41 @@ using System.Threading.Tasks;
 
 namespace ClientAdmin
 {
-
-    public class Credentials
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-
-    public class Session
-    {
-        public string TeacherId { get; set; }
-        public string Id { get; set; }
-    }
-
     class Program
     {
         public static HttpClient WebClient = new HttpClient();
         public static string token;
         static void Main(string[] args)
         {
-            Program program = new Program();
-            program.HandleLogin();
+            _ = Run();
+        }
+
+        private static async Task Run()
+        {
+            while (true)
+            {
+                if (token == null)
+                {
+                    Program program = new Program();
+                    program.HandleLogin();
+                } else
+                {
+                    Console.WriteLine("**************  Menu ***************");
+                    Console.WriteLine("1: View Logs ");
+                    string strOption = Console.ReadLine();
+                    int option = Int32.Parse(strOption);
+                    switch (option)
+                    {
+                        case 1:
+                            HandleViewLogs();
+                            break;
+                        default:
+                            break;
+                    }
+                    Console.WriteLine("");
+                }
+                
+            }
         }
 
         private void HandleLogin()
@@ -50,10 +65,68 @@ namespace ClientAdmin
             {
                 Console.WriteLine(httpResponseMsg.Content.ReadAsStringAsync());
             }
+        }
 
-            //HttpResponseMessage webResponse = WebClient.GetAsync(endpoint).Result;
+        private static void HandleViewLogs()
+        {
+            Console.WriteLine("**************  Logs Menu  *****************");
+            Console.WriteLine("1: View all ");
+            Console.WriteLine("2: Alta alumnos");
+            Console.WriteLine("3: Alta docentes");
+            Console.WriteLine("4: Alta curso");
+            Console.WriteLine("5: Baja curso");
+            Console.WriteLine("6: Inscripciones a cursos");
+            Console.WriteLine("7: Coreccion de materiales");
+            string strOption = Console.ReadLine();
+            int option = Int32.Parse(strOption);
+            switch (option)
+            {
+                case 1:
+                    HandleRequestLogs("All");
+                    break;
+                case 2:
+                    HandleRequestLogs("CreateStudent");
+                    break;
+                case 3:
+                    HandleRequestLogs("CreateCourse"); // Cambiar por docentes.
+                    break;
+                case 4:
+                    HandleRequestLogs("CreateCourse");
+                    break;
+                case 5:
+                    HandleRequestLogs("DeleteCourse");
+                    break;
+                case 6:
+                    HandleRequestLogs("Inscription");
+                    break;
+                case 7:
+                    HandleRequestLogs("All");
+                    break;
+                default:
+                    break;
+            }
 
-            Console.ReadLine();
+        }
+        private static void HandleRequestLogs(string filter)
+        {
+            using (LogServices.LogServiceClient client = new LogServices.LogServiceClient())
+            {
+                try
+                {
+                    var logs = client.GetLogs(filter);
+                    foreach (var item in logs)
+                    {
+                        Console.WriteLine(item.Message);
+
+                    }
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
         }
     }
 }
